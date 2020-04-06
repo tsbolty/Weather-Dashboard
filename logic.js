b2e190d1183e38eecadd69f415739fb9a6f80948
Tyler Bolty <tsbolty@gmail.com>
2020-04-06 09:50:21 -0600

    very close to being finished. still quirks with the day/ night icons and inconsistency with the on click events

--- logic.js
+++ logic.js
@@ -8,35 +8,35 @@ $("#search-btn").on("click", function(event){
     $("#search-text").val("")
     searchWeather(cityName)
 })
-    function searchWeather(search){
-        $.ajax({
-            type: "GET",
-            url: `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=imperial&appid=5daa520299ac74c142ce2d492d9e2ed7`,
-            dataType: "json"
-        }).then(function(data){
-            if (searches.includes(search)){
-                console.log("this has been included")
-            } else{
-                createBtn(search)
-            }
-            searches.push(search)
-            $("#city-name").text(search+ " " +moment().format("dddd, MMMM Do YYYY"))
-            // temp
-            $("#temp").text("Temperature: " +data.main.temp+ "° Fahrenheit")
-            // humidity
-            $("#humidity").text("Humidity: "+data.main.humidity+"%")
-            // wind speed
-            $("#wind-speed").text("Wind: " +data.wind.speed+" mph")
-            forecast(search)
-            uvSearch(data.coord.lat, data.coord.lon)
-
-        })
-    }
+function searchWeather(search){
+    console.log("search term: "+search)
+    $.ajax({
+        type: "GET",
+        url: `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=imperial&appid=5daa520299ac74c142ce2d492d9e2ed7`,
+        dataType: "json"
+    }).then(function(data){
+        if (searches.includes(search)){
+            console.log("this has been included")
+        } else{
+            createBtn(search)
+        }
+        searches.push(search)
+        
+        $("#city-name").text(search+ " " +moment().format("dddd, MMMM Do YYYY"))
+        // temp
+        $("#temp").text("Temperature: " +data.main.temp+ "° Fahrenheit")
+        // humidity
+        $("#humidity").text("Humidity: "+data.main.humidity+"%")
+        // wind speed
+        $("#wind-speed").text("Wind: " +data.wind.speed+" mph")
+        forecast(search)
+        uvSearch(data.coord.lat, data.coord.lon)
+    })
+}
 
 
 $(".history").on("click", "li", function(event){
     event.preventDefault()
-    console.log(this)
     searchWeather($(this).text())
 })
 
@@ -75,10 +75,11 @@ function forecast(search){
         dataType: "json"
     }).then(function(data){
         for(i=0; i<6; i++){
-            console.log([i])
+            
             $("#5day-forecast").text("5 Day Forecast")
             var forecastTemp = data.list[i].main.temp
             var forecastHumidity = data.list[i].main.humidity
+            var weatherIcon = data.list[i].weather[0].icon
             var oneDayForward = new moment().add(1, 'day');
             var twoDayForward = new moment().add(2, 'day');
             var threeDayForward = new moment().add(3, 'day');
@@ -89,7 +90,8 @@ function forecast(search){
             $("#day3-date").text(threeDayForward.format('dddd MMMM DD'))
             $("#day4-date").text(fourDayForward.format('dddd MMMM DD'))
             $("#day5-date").text(fiveDayForward.format('dddd MMMM DD'))
-            $(`#day${[i]}-temp`).text(`Temperature: ${forecastTemp}Fahrenheit`)
+            $(`#day${[i]}-icon`).attr("src", `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`)
+            $(`#day${[i]}-temp`).text(`Temperature: ${forecastTemp}° Fahrenheit`)
             $(`#day${[i]}-humidity`).text(`Humidity: ${forecastHumidity}%`)
         }
     })
